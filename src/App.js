c3cdbc9fa755edbb1b9cbb95f735355592486209
Ed Lewis <ed.lewis@gmail.com>
2018-01-22 13:44:14 -0500

    lots of fixes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import WordSearch from "./wordSearch"
 import WordSearchDisplay from './WordSearchDisplay.react'
+import Textarea from "react-textarea-autosize";
 
 class App extends Component {
 
@@ -9,7 +10,7 @@ class App extends Component {
     super(props)
 
     this.state = {
-      wordText: "",
+      wordText: "cat\ndog\nbird",
       results: null
     }
   }
@@ -26,9 +27,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
-        <div onClick={this.makeWordSearch.bind(this)}>make a word search</div>
-        <textarea onChange={this.updateWords.bind(this)} value={this.state.wordText}/>
+        <div className='no-print'>
+          <div>Add your list of words here, separated by line breaks</div>
+          <div>
+          <Textarea
+            useCacheForDOMMeasurements
+            value={this.state.wordText}
+            className="word-input"
+            onChange={this.updateWords.bind(this)}
+            />
+        </div>
+          <div className="button-make" onClick={this.makeWordSearch.bind(this)}>make a word search</div>
+        </div>
         {this.state.results && <WordSearchDisplay results={this.state.results} wordList={this.state.wordText.split("\n")} /> }
       </div>
     );
