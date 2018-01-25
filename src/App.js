e8cb08d4e89ea144fc9c3539d9fed683b192ab6f
Ed Lewis <ed.lewis@gmail.com>
2018-01-25 17:17:35 -0500

    hide url when grid is generated

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends Component {
   makeWordSearch() {
     let wordList = this.state.wordText.split("\n")
     wordList = removeArrayRepeats(wordList)
-    this.setState({ results: WordSearch(wordList), wordList: wordList, canSave: true })
+    this.setState({ results: WordSearch(wordList), wordList: wordList, canSave: true, url: null })
   }
 
   saveGrid() {
