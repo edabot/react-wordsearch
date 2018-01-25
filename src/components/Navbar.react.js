af39834ad2c027d1eb45ae2dbdbed5db53203373
Ed Lewis <ed.lewis@gmail.com>
2018-01-25 14:12:30 -0500

    fix css for print

--- src/components/Navbar.react.js
+++ src/components/Navbar.react.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const navbar = () => (
-  <div className='navbar'>
+  <div className='navbar no-print'>
     <div className='navbar_left'><Link to="/">Home</Link></div>
     <div className='navbar_right'>
       <Link to="/wordsearches">Word Searches</Link>
