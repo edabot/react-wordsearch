19dc1299f986d9c341056d5c562752f2de0aa5c5
Ed Lewis <ed.lewis@gmail.com>
2018-02-27 15:10:32 -0500

    add existing word searches to list

--- src/components/Lists.react.js
+++ src/components/Lists.react.js
@@ -1,78 +1,68 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
-import { Link } from 'react-router-dom'
-import {List, ListItem} from 'material-ui/List';
+import { Link } from 'react-router-dom';
+import { List, ListItem } from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 
 const style = {
   margin: 20,
-  display: 'inline-block',
+  display: 'inline-block'
 };
 
-const GridItem = ({item}) => (
+const GridItem = ({ item }) => (
   <div>
-    <Link to={item.id} ><ListItem primaryText={item.name} /></Link>
+    <Link to={item.id}>
+      <ListItem primaryText={item.name} />
+    </Link>
   </div>
-)
+);
 
-const GridList = ({list}) => (
+const GridList = ({ list }) => (
   <div>
     <Paper style={style} zDepth={1}>
       <Subheader>{list.name}</Subheader>
       <List>
-        {list.grids.map(item=> <GridItem item={item} key={item.name}/>)}
+        {list.grids.map(item => <GridItem item={item} key={item.name} />)}
       </List>
     </Paper>
   </div>
-)
+);
 
 class Lists extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       lists: [
         {
-          name: "cartoons",
+          name: 'cartoons',
           grids: [
-            {name: "Pokemon", id: ""},
-            {name: "Simpsons", id: ""},
-            {name: "Akira", id: ""},
-            {name: "Beyblade", id: ""},
+            { name: 'Pokemon', id: 'SJMAKVQ_G' },
+            { name: 'Simpsons', id: 'SJv2cNQOz' }
           ]
         },
         {
-          name: "movies",
+          name: 'movies',
           grids: [
-            {name: "Star Wars", id: ""},
-            {name: "Star Trek", id: ""},
-            {name: "Top Gun", id: ""},
-            {name: "Muppets", id: ""},
-          ]
-        },
-        {
-          name: "TV shows",
-          grids: [
-            {name: "TMNT", id: ""},
-            {name: "Smurfs", id: ""},
-            {name: "A-Team", id: ""},
-            {name: "ST:TNG", id: ""},
+            { name: 'Star Wars', id: 'BJBlj4mdz' },
+            { name: 'Star Trek', id: 'Hkl1u4Xuf' },
+            { name: 'Muppets', id: 'SkzKu47uf' }
           ]
         }
       ]
-    }
+    };
   }
 
   render() {
     return (
       <div className="lists-content">
-        This page will store lists of pre-made grids. These lists are currently for display only and all links go to the word search generator.
         <div className="lists-display">
-          {this.state.lists.map(list=><GridList list={list} key={list.name} />)}
+          {this.state.lists.map(list => (
+            <GridList list={list} key={list.name} />
+          ))}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Lists
+export default Lists;
